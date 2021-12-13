e3b3ef21a7c0b849c2656c256e6b485b84ff1961
rahmiirasfauziah <1911521004_rahmi@student.unand.ac.id>
2021-12-13 23:25:36 +0700

    login

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity implements login {
+
+    private Bundle savedInstanceState;
+    private Object RelativeLayout;
+
+    @Override
+    protected void onCreate() {
+        onCreate();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.savedInstanceState = savedInstanceState;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        char login;
+
+        String id;
+        String layout_width;
+        String layout_height;
+        String layout_clipToOutline;
+        String clipToOutline;
+        String background;
+
+        String android;
+        xmlns:android="http://schemas.android.com/apk/res/android";
+        android:id="@+id/login";
+        android:layout_width="fill_parent";
+        android:layout_height="fill_parent";
+        android:clipToOutline="true";
+        android:background="@drawable/login";
+
+Object vector;
+        String aapt;
+        String width;
+        String height;
+        String viewportWidth;
+        String viewportHeight;
+
+        xmlns:android="http://schemas.android.com/apk/res/android";
+        xmlns:aapt="http://schemas.android.com/aapt";
+        android:width="375dp";
+        android:height="812dp";
+        android:viewportWidth="375";
+        android:viewportHeight="812";
+
+<Object group;
+        group/>
+
+<Object path;
+        Object clip;
+        clip-path;
+        String pathData;
+        android:pathData="M20 0H355C366.046 0 375 8.95431 375 20V792C375 803.046 366.046 812 355 812H20C8.95431 812 0 803.046 0 792V20C0 8.95431 8.95431 0 20 0Z";
+                />
+
+<path
+        String pathData;
+        String fillColor;
+        android:pathData="M0 0V812H375V0";
+        android:fillColor="#F2F2F2";
+                />
+    }
+}
